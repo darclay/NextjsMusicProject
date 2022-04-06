01312b52c8c124f82078ef6b1d498ac9fd587ad6
David Gurule <davidgurulejr@gmail.com>
2022-04-06 16:18:52 -0600

    created a header and footer & added them to layout

---
+++ music-app-frontend/components/Footer.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Music Events Copyright &copy; 2022</p>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <br/>
+      <a href="http://www.david-barclay-gurule.com" target="_blank">D. Gurule</a>
+    </footer>
+  )
+}
--- music-app-frontend/components/Layout.js
+++ music-app-frontend/components/Layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Layout.module.css'
+import Header from './Header'
+import Footer from './Footer'
 
 export default function Layout({ title, keywords, description, children }) {
   
@@ -10,9 +12,12 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description}></meta>
         <meta name="keywords" content={keywords}></meta>
       </Head>
-      <div className={styles.container}>
-        {children}
-      </div> 
+      <Header>
+        <div className={styles.container}>
+          {children}
+        </div> 
+      </Header>
+      <Footer></Footer>
     </div>
   )
 }
